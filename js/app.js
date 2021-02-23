7982cf02d7c8a7d5327fc51d870a702d57ae911b
dianaseul <58045169+dianaseul@users.noreply.github.com>
2021-02-23 15:56:41 -0800

    lightbox work

--- js/app.js
+++ js/app.js
@@ -1,2 +1,10 @@
-baguetteBox.run('.gallery');​
+baguetteBox.run('.gallery', {
+    captions: true,
+    buttons: auto,
+    fullScreen: false,
+    noScrollBars: false,
+    bodyClass: 'baguetteBox-open',
+    async: false,
+    overlayBackgroundColor: rgba(0, 0, 0, .8),
+});​
 const search = new Filter('search', 'data-caption');​
