dab63c0519839d7d6dca3720ff2f793f39bb277d
dianaseul <58045169+dianaseul@users.noreply.github.com>
2021-02-24 12:07:31 -0800

    Search filter
    search added and working

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 baguetteBox.run('.gallery', {
     captions: true,
-    buttons: auto,
+    buttons: 'auto',
     fullScreen: false,
     noScrollBars: false,
     bodyClass: 'baguetteBox-open',
     async: false,
-    overlayBackgroundColor: rgba(0, 0, 0, .8),
-});​
-const search = new Filter('search', 'data-caption');​
+    overlayBackgroundColor: 'rgba(0, 0, 0, .8)',
+});
+
+
+const search = new Filter('search', 'data-caption');
