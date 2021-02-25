78a0712b5e96fe8ff27eb7c4390334519381114c
dianaseul <58045169+dianaseul@users.noreply.github.com>
2021-02-25 14:43:01 -0800

    own search function
    own search function added and working

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+
+// baguetteBox lightbox plugin //
 baguetteBox.run('.gallery', {
     captions: true,
     buttons: 'auto',
@@ -9,4 +11,28 @@ baguetteBox.run('.gallery', {
 });
 
 
-const search = new Filter('search', 'data-caption');
+// Create my own search function to filter images by key words //
+const search = document.getElementById('search');
+
+search.addEventListener('keyup', (e) => { // registers each key stroke //
+    const userSearch = e.target.value.toLowerCase(); // search string to lowercase //
+    console.log('searchkey', userSearch);
+    mySearchFunction(); // calls for loop //
+});
+
+function mySearchFunction() {
+    var input, filter, gallery, a, data;
+    input = document.getElementById('search');
+    filter = input.value.toLowerCase();
+    gallery = document.getElementsByClassName('gallery');
+    a = gallery[0].getElementsByTagName('a');
+    
+    for (var i = 0; i < a.length; i++) {
+        data = a[i].getAttribute('data-caption');
+        if (data.indexOf(filter) > -1) {
+            a[i].style.display = '';
+        } else {
+            a[i].style.display = 'none';
+        } 
+    };
+} 
