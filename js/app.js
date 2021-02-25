0105a8418e8316ec2606b93921dc7630b71b930a
dianaseul <58045169+dianaseul@users.noreply.github.com>
2021-02-25 15:42:12 -0800

    Code Cleanup
    cleanup & comments added

--- js/app.js
+++ js/app.js
@@ -1,32 +1,28 @@
-
 // baguetteBox lightbox plugin //
+// --------------------------- //
 baguetteBox.run('.gallery', {
     captions: true,
     buttons: 'auto',
-    fullScreen: false,
-    noScrollBars: false,
-    bodyClass: 'baguetteBox-open',
-    async: false,
-    overlayBackgroundColor: 'rgba(0, 0, 0, .8)',
 });
 
 
 // Create my own search function to filter images by key words //
+// ---------------------------------------------------------- //
 const search = document.getElementById('search');
 
-search.addEventListener('keyup', (e) => { // registers each key stroke //
-    const userSearch = e.target.value.toLowerCase(); // search string to lowercase //
+search.addEventListener('keyup', (e) => { // registers each key stroke 
+    const userSearch = e.target.value.toLowerCase(); // search keystrokes to lowercase 
     console.log('searchkey', userSearch);
-    mySearchFunction(); // calls for loop //
+    mySearchFunction(); // calls for loop 
 });
 
 function mySearchFunction() {
     var input, filter, gallery, a, data;
     input = document.getElementById('search');
-    filter = input.value.toLowerCase();
+    filter = input.value.toLowerCase(); // search field input string to all lowercase
     gallery = document.getElementsByClassName('gallery');
     a = gallery[0].getElementsByTagName('a');
-    
+    //loop if search string filter matches string found in data, display link & if not display none.
     for (var i = 0; i < a.length; i++) {
         data = a[i].getAttribute('data-caption');
         if (data.indexOf(filter) > -1) {
